7d32a649205b470c6a9ee35db36aeecca10fb1ba
Holden Vail <112910214+HoldenV@users.noreply.github.com>
2024-10-30 12:33:02 -0500

    update names in seeds

--- seeds/seeds.h
+++ seeds/seeds.h
@@ -3,9 +3,9 @@
 
 // Function prototypes
 int* read_start_pattern(char* filename, int max_width, int max_height);
-int* gol_gen_next(int* pattern, int width, int height);
-int* gol_gen_random(int width, int height, int percent_alive);
-void gol_add_life(int* pattern, int width, int height, int percent_alive);
-int gol_count_live_neighbors(int* pattern, int width, int height, int cell_index);
+int* seeds_gen_next(int* pattern, int width, int height);
+int* seeds_gen_random(int width, int height, int percent_alive);
+void seeds_add_life(int* pattern, int width, int height, int percent_alive);
+int seeds_count_live_neighbors(int* pattern, int width, int height, int cell_index);
 
 #endif
